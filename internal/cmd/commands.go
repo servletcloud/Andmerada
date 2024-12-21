4454c78a661e55bdb413f9a265677533d3019b13
Vladimir Korobkov <vladimir.korobkov@gmail.com>
2024-12-21 15:55:02 +0200

    grouped text messages into .txt files to run a spellchecker on them

--- internal/cmd/commands.go
+++ internal/cmd/commands.go
@@ -5,38 +5,19 @@ import (
 	"log"
 	"os"
 
+	"github.com/servletcloud/Andmerada/internal/resources"
 	"github.com/spf13/cobra"
 )
 
-const msgProjectAlreadyExists string = `Error: Project initialization failed.
-The file 'andmerada.yml' already exists in the specified directory.
-
-Suggestion:
-- If this is an existing Andmerada project, you can start by running commands like:
- andmerada create-migration "Add users table"
-
-- If you want to reinitialize the project, please remove or back up the existing 'andmerada.yml' file and try again.`
-
-const msgInitCompleted string = `Next Steps:
-1. Configure your project:
-  Edit 'andmerada.yml' to update the project name, migrations table name, and other settings.
-
-2. Create your first migration:
-  andmerada create-migration "Add users table"
-
-3. Run your first migration:
-  andmerada migrate`
-
 func initCommand() *cobra.Command {
+	description := resources.LoadInitCommandDescription()
+
 	//nolint:exhaustruct
 	return &cobra.Command{
-		Use:   "init [directory]",
-		Short: "Initialize a new Andmerada migration project",
-		Long: `The 'andmerada init' command sets up a new migration project.
-If no directory is provided, the current directory will be used.
-It pre-creates the specified directory (including nested folders) if it does not
-already exist and generates an 'andmerada.yml' configuration file.`,
-		Args: cobra.MaximumNArgs(1),
+		Use:   description.Use,
+		Short: description.Short,
+		Long:  description.Long,
+		Args:  cobra.MaximumNArgs(1),
 		Run: func(_ *cobra.Command, args []string) {
 			var targetDir string
 			if len(args) > 0 {
@@ -51,13 +32,13 @@ already exist and generates an 'andmerada.yml' configuration file.`,
 
 			if err := initializeProject(targetDir); err != nil {
 				if errors.Is(err, errConfigFileAlreadyExists) {
-					log.Fatalln(msgProjectAlreadyExists)
+					log.Fatalln(resources.MsgErrProjectExists())
 				}
 				log.Panic(err)
 			}
 
 			log.Printf("Project initialized successfully in %v", targetDir)
-			log.Println(msgInitCompleted)
+			log.Println(resources.MsgInitCompleted())
 		},
 	}
 }
--- internal/resources/resources.go
+++ internal/resources/resources.go
@@ -2,11 +2,53 @@ package resources
 
 import (
 	_ "embed"
+	"strings"
+)
+
+type CommandDescription struct {
+	Use   string
+	Short string
+	Long  string
+}
+
+const (
+	unixNewLine = "\n"
 )
 
 //go:embed template.andmerada.yml
 var templateAndmeradaYml []byte
 
+//go:embed command_init_description.txt
+var commandInitDiscription []byte
+
+//go:embed msg_init_completed.txt
+var msgInitCompleted []byte
+
+//go:embed msg_err_project_exists.txt
+var msgErrProjectExists []byte
+
 func TemplateAndmeradaYml() string {
 	return string(templateAndmeradaYml)
+}
+
+func LoadInitCommandDescription() CommandDescription {
+	return loadCommandDescription(commandInitDiscription)
+}
+
+func MsgInitCompleted() string {
+	return string(msgInitCompleted)
+}
+
+func MsgErrProjectExists() string {
+	return string(msgErrProjectExists)
+}
+
+func loadCommandDescription(s []byte) CommandDescription {
+	lines := strings.Split(string(s), unixNewLine)
+
+	return CommandDescription{
+		Use:   lines[0],
+		Short: lines[1],
+		Long:  strings.Join(lines[2:], unixNewLine),
+	}
 }
