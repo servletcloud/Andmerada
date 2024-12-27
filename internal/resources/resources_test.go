69d4021a1a0c6d101da5ee912427d89e8406dabc
servletcloud <vovan@hey.com>
2024-12-27 23:59:03 +0200

    Added test to validate that migration.yml matches the schema (#8)
    Co-authored-by: Vladimir Korobkov <vladimir.korobkov@gmail.com>

--- internal/resources/resources_test.go
+++ internal/resources/resources_test.go
@@ -1,11 +1,15 @@
 package resources_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/servletcloud/Andmerada/internal/resources"
 	"github.com/servletcloud/Andmerada/internal/tests"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/xeipuuv/gojsonschema"
+	"gopkg.in/yaml.v3"
 )
 
 func TestTemplateAndmeradaYml(t *testing.T) {
@@ -24,6 +28,31 @@ func TestTemplateMigrationYml(t *testing.T) {
 
 	assert.Contains(t, content, `name: "Create users table(!)"`)
 	tests.AssertPlaceholdersResolved(t, content)
+}
+
+func TestMigrationYMLTemplateMatchesSchema(t *testing.T) {
+	t.Parallel()
+
+	yamlFile := resources.TemplateMigrationYml("Create users table")
+
+	var yamlData map[string]interface{}
+
+	require.NoError(t, yaml.Unmarshal([]byte(yamlFile), &yamlData))
+
+	jsonData, err := json.Marshal(yamlData)
+	require.NoError(t, err)
+
+	schemaLoader := gojsonschema.NewReferenceLoader("file://./../../api/schema/migration.yml.v1.json")
+	documentLoader := gojsonschema.NewStringLoader(string(jsonData))
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	require.NoError(t, err)
+
+	assert.True(t, result.Valid(), "migration.yml template does not match the schema")
+
+	for _, desc := range result.Errors() {
+		t.Errorf("Schema validation error: %s", desc)
+	}
 }
 
 func TestMsgMigrationCreated(t *testing.T) {
