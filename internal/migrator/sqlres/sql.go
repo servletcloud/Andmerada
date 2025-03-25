4854e19c0af075faf6908756fae3178b138e0cf2
servletcloud <vovan@hey.com>
2025-03-25 20:44:30 +0200

    Made registering an applied migration be idempotent upsert  (#30)
    It removes possible conflicts.
    
    Co-authored-by: Vladimir Korobkov <vladimir.korobkov@gmail.com>

--- internal/migrator/sqlres/sql.go
+++ internal/migrator/sqlres/sql.go
@@ -8,6 +8,13 @@ import (
 //go:embed ddl.sql
 var ddl string
 
+//go:embed register-migration.sql
+var registerMigrationQuery string
+
 func DDL(appliedMigrationsName string) string {
 	return strings.ReplaceAll(ddl, "_applied_migrations_", appliedMigrationsName)
+}
+
+func RegisterMigrationQuery(tableName string) string {
+	return strings.ReplaceAll(registerMigrationQuery, "__table_name__", tableName)
 }
